458cb38bed7b022cc9aa776db50d612b75ed16c0
Charls-duno <c20246360.busico@csav.edu.ph>
2026-09-11 09:02:15 +0800

    variables(let & const)

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Hello, World!");
+const Name = "Jose Charls M. Busico"; 
+console.log("Name is: " + Name); 
+
+let Age = 20; 
+console.log("Age is: " + Age); 
+let Number = 09166138837; 
+console.log("Number is: " + Number); 
+let Address = "Victorias City, Philippines"; 
+console.log("Address is: " + Address); 
